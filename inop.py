bbb1e9fb341b3db5edd56ed13b2623a3f6272c04
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 08:20:28 +0200

    HW!!!

---
+++ inop.py
@@ -0,0 +1,10 @@
+  #str, list,dict...
+
+#HW: what if digit input < int
+digit = 1
+forbiddon_digits = "789"
+
+if str(digit) in forbiddon_digits:
+    print("Can' use", digit)
+else :
+    print ("Can't be used")
